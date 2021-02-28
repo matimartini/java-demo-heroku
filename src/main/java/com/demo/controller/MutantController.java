fb363250abd2d1398f9f76c7f88852fc025a5d82
Matias Gabriel Martini <matias.martini.gabriel@gmail.com>
2021-02-28 18:54:17 -0300

    initial app demo

---
+++ src/main/java/com/demo/controller/MutantController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mutant")
+public class MutantController {
+
+
+    @GetMapping("/ping")
+    public String getPong(){
+        return "pong";
+    }
+}
